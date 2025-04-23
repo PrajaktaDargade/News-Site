adb339174edb776b0c4b4ceb1ca12eea7bbd15a6
Prajakta Dargade <prajudargade@gmail.com>
2025-04-23 12:20:46 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "http://localhost/news-site";
+
+$conn = mysqli_connect("localhost","prajakta","Praju@123","news-site") or die("Connection failed : " . mysqli_connect_error());
+
+?>
